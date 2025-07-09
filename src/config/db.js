b22d0baa2b677e207f711cc0638cf3860247c6af
Oscar Mallma <oscarmallma931@gmail.com>
2025-07-08 19:15:22 -0500

    Update env mongo and db

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function conect() {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(`${process.env.MONGO_URL}/${process.env.DB_NAME}`);
     console.log("Conectado a MongoDB");
   } catch (error) {
     console.error("Error al conectar a MongoDB:", error);
