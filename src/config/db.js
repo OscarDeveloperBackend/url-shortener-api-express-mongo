85b1871dc0932ea3f8e1b8936ae90fa574504480
Oscar Mallma <oscarmallma931@gmail.com>
2025-07-09 16:08:26 -0500

    Cambios 01

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 async function conect() {
   try {
-    await mongoose.connect(`${process.env.MONGO_URL}/${process.env.DB_NAME}`);
+    await mongoose.connect(process.env.MONGO_URL, {
+      dbname: process.env.DB_NAME,
+    });
     console.log("Conectado a MongoDB");
   } catch (error) {
     console.error("Error al conectar a MongoDB:", error);
